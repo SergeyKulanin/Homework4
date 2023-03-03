756eac7d9eb78e1300ee71413046eff309c68191
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-03 20:47:37 +0300

    Сумма цифр в числе_исправление

--- Ex27/Program.cs
+++ Ex27/Program.cs
@@ -1,15 +1,17 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
 
-Console.Clear();
-Console.Write("Введите число: ");
-int i = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int num = 0;
+int SumDigitsInNumber(int number)
+{
+    int result = 0;
+    while(number > 0)
+        {
+        result = result + number % 10;
+        number = number / 10;
+        }
+    
+    return result;
+}
 
-while(i > 0)
-    {
-        num = i % 10;
-        sum = sum + num;
-        i = i / 10;
-    }
-Console.WriteLine($"сумма цифр в числе равна: {sum}");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigitsInNumber(number)}");
