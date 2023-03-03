793c28a9d7347b84e6a63ccbc79bc5dcc9007404
Sergey Kulanin <sjulanin1981@gmail.com>
2023-03-03 21:23:27 +0300

    Возведение в степень_исправлено

--- Ex25/Program.cs
+++ Ex25/Program.cs
@@ -1,6 +1,18 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
 // возводит число A в натуральную степень B.
 
+int RaiseToPower(int A, int B)
+{
+    int i = 1;
+    int result = A;
+    while (i < B)
+    {
+        result = result * A;
+        i++;
+    }
+    return result;
+}
+
 Console.Clear();
 Console.WriteLine("Введите число A: ");
 int A = Convert.ToInt32(Console.ReadLine());
@@ -8,12 +20,4 @@ int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 
-int result = A;
-int i = 1;
-while( i < B)
-    {
-        result = result * A;
-        i++;
-    }
-    
-Console.WriteLine($"{A} в степени {B} равно {result}");
+Console.WriteLine($"{A} в степени {B} равно {RaiseToPower(A, B)}");
